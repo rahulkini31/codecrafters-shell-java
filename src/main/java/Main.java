df150d4e0e7a379e8fc299734c299274ec3bea42
Rahul Kini <rahulkini369@gmail.com>
2025-01-15 16:39:08 +0530

    [type builtin]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,26 +2,6 @@
 //
 //public class Main {
 //    public static void main(String[] args) throws Exception {
-//        // Uncomment this block to pass the first stage
-//         System.out.print("$ ");
-//
-//        Scanner scanner = new Scanner(System.in);
-//        String input = scanner.nextLine();
-//
-//        do {
-//            System.out.println(input + ": command not found");
-//
-//            System.out.print("$ ");
-//
-//            input = scanner.nextLine();
-//        } while ( !input.matches(""));
-//        scanner.close();
-//    }
-//}
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main(String[] args) throws Exception {
 //        System.out.print("$ ");
 //
 //        Scanner scanner = new Scanner(System.in);
@@ -36,6 +16,8 @@
 //                } else {
 //                    System.out.println("Usage: exit 0");
 //                }
+//            } else if (input.startsWith("echo")) {
+//                System.out.println(input.substring(5));
 //            } else {
 //                System.out.println(input + ": command not found");
 //            }
@@ -66,6 +48,18 @@ public class Main {
                 }
             } else if (input.startsWith("echo")) {
                 System.out.println(input.substring(5));
+            } else if (input.startsWith("type")) {
+                String[] parts = input.split(" ");
+                if (parts.length == 2) {
+                    String command = parts[1];
+                    if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
+                        System.out.println(command + " is a shell builtin");
+                    } else {
+                        System.out.println(command + ": not found");
+                    }
+                } else {
+                    System.out.println("Usage: type <command>");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }

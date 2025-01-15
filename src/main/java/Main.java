d8607933d791a2f2822521f796e883395fadd3ee
Rahul Kini <rahulkini369@gmail.com>
2025-01-15 16:40:16 +0530

    [exe at path type builtin]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,18 @@
 //                }
 //            } else if (input.startsWith("echo")) {
 //                System.out.println(input.substring(5));
+//            } else if (input.startsWith("type")) {
+//                String[] parts = input.split(" ");
+//                if (parts.length == 2) {
+//                    String command = parts[1];
+//                    if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
+//                        System.out.println(command + " is a shell builtin");
+//                    } else {
+//                        System.out.println(command + ": not found");
+//                    }
+//                } else {
+//                    System.out.println("Usage: type <command>");
+//                }
 //            } else {
 //                System.out.println(input + ": command not found");
 //            }
@@ -28,6 +40,7 @@
 //    }
 //}
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -55,7 +68,20 @@ public class Main {
                     if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
                         System.out.println(command + " is a shell builtin");
                     } else {
-                        System.out.println(command + ": not found");
+                        String path = System.getenv("PATH");
+                        String[] directories = path.split(":");
+                        boolean found = false;
+                        for (String dir : directories) {
+                            File file = new File(dir, command);
+                            if (file.exists() && file.canExecute()) {
+                                System.out.println(command + " is " + file.getAbsolutePath());
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found) {
+                            System.out.println(command + ": not found");
+                        }
                     }
                 } else {
                     System.out.println("Usage: type <command>");

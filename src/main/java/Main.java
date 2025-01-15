cd3f86cbe948bf0a9146b479f4aec8d5d6c3cb7a
Rahul Kini <rahulkini369@gmail.com>
2025-01-15 16:37:58 +0530

    [echo builtin]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,34 @@
 //        scanner.close();
 //    }
 //}
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) throws Exception {
+//        System.out.print("$ ");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//
+//        while (true) {
+//            if (input.startsWith("exit")) {
+//                String[] parts = input.split(" ");
+//                if (parts.length == 2 && parts[1].equals("0")) {
+//                    scanner.close();
+//                    System.exit(0);
+//                } else {
+//                    System.out.println("Usage: exit 0");
+//                }
+//            } else {
+//                System.out.println(input + ": command not found");
+//            }
+//
+//            System.out.print("$ ");
+//            input = scanner.nextLine();
+//        }
+//    }
+//}
+
 import java.util.Scanner;
 
 public class Main {
@@ -36,6 +64,8 @@ public class Main {
                 } else {
                     System.out.println("Usage: exit 0");
                 }
+            } else if (input.startsWith("echo")) {
+                System.out.println(input.substring(5));
             } else {
                 System.out.println(input + ": command not found");
             }
